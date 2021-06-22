7b0b33de00b29654766e7691613c9c2326282348
szyilmaz <szyilmaz@yukselproje.com.tr>
2021-06-22 08:37:52 +0300

    added redux adn implemented userReducer and userActions to header

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import store from './redux/store';
+
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
