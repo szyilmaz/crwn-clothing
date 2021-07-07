c846cced3358332912531214431265b530145abd
szyilmaz <szyilmaz@yukselproje.com.tr>
2021-07-07 18:06:26 +0300

    yes

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,30 +1,23 @@
-import {Component} from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
-import SECTIONS_DATA from "./sections.data.js";
 
-class Directory extends Component {
- constructor() {
-     super();
+const Directory = ({sections}) => (
+    <div className='directory-menu'>
+    {
+       sections.map(({id, ...otherSectionProps}) => (
+           <MenuItem key={id} {...otherSectionProps} />
+       ))
+    }
+    </div>
+);
 
-     this.state = {
-        sections: SECTIONS_DATA
-     };
- }
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+});
 
- render() {
-     return (
-         <div className='directory-menu'>
-         {
-            this.state.sections.map(({id, ...otherSectionProps}) => (
-                <MenuItem key={id} {...otherSectionProps} />
-            ))
-         }
-         </div>
-     )
- }
-
-
-}
-
-export default Directory;
+export default connect(mapStateToProps)(Directory);
--- src/index.js
+++ src/index.js
@@ -4,19 +4,22 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
