fb2f0dab83127088e45007bf54606e1a164cf08e
szyilmaz <szyilmaz@yukselproje.com.tr>
2021-07-12 08:41:46 +0300

    make it better

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,7 +13,7 @@ const config = {
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if(!userAuth) return;
+    if(!userAuth) return; 
 
     const userRef = firestore.doc(`users/${userAuth.uid}`);
     const snapShot = await userRef.get();
